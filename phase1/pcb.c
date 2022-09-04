7e12baf961c867574b60b7811b52342e0fa095e9
Travis Wahl <twahl1991@gmail.com>
2022-09-04 17:38:17 -0400

    Update pcb.c
    added book description and beginning method of insertBlocked

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -328,6 +328,18 @@ pcb_t *outChild(pcb_t *p){
 
 // ---------------- 2.4 The Active Semaphore List (ASL) ------------------------
 
+/* Insert the pcb pointed to by p at the tail of the process queue associated with the
+semaphore whose physical address is semAdd and set the semaphore address of p to semAdd.
+If the semaphore is currently not active (i.e. there is no descriptor for it in the ASL),
+allocate a new descriptor from the semdFree list, insert it in the ASL (at the appropriate
+position), initialize all of the fields (i.e. set s_semAdd to semAdd, and s_procQ to
+mkEmptyProcQ()), and proceed as above. If a new semaphore descriptor needs to be allocated
+and the semdFree list is empty, return TRUE. In all other cases return FALSE. */
+int insertBlocked (int *semAdd, pcb_t *p) {
+	//code
+}
+
+
 /* Search the ASL for a descriptor of this semaphore. If none is found, return NULL;
 otherwise, remove the first (i.e. head) pcb from the process queue of the found semaphore descriptor and
 return a pointer to it. If the process queue for this semaphore becomes empty (emptyProcQ(s procq) is TRUE),
@@ -336,13 +348,13 @@ pcb_t *removeBlocked(int *semAdd) {
 	//code
 }
 
+	
 /* Remove the pcb pointed to by p from the process queue associated with p’s semaphore
 (p→ p semAdd) on the ASL. If pcb pointed to by p does not appear in the process queue associated
 with p’s semaphore, which is an error condition, return NULL; otherwise, re- turn p. */
 pcb_t *outBlocked(pcb t *p) {
 	// code
 }
-
 
 
 /* Return a pointer to the pcb that is at the head of the process queue associated with the semaphore
