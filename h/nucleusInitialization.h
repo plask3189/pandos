88c4bd7ee7266755285ce4d9d05b03b95b9b3e7e
plask3189 <kateplas45208@gmail.com>
2022-09-21 11:44:50 -0400

    Create nucleusInitialization.h

---
+++ h/nucleusInitialization.h
@@ -0,0 +1,12 @@
+/* nucleusInitialization.h */
+
+#define nucleusInitialization
+
+extern int processCount;
+extern int softBlockCount;
+extern pcb_PTR currentProcess;
+extern pcb_PTR readyQueue;
+extern int deviceSemaphores[NUMBEROFDEVICES];
+
+
+#endif
