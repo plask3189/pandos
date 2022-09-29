4b4266973898562e876d095dfe6a169aab0fc392
plask3189 <kateplas45208@gmail.com>
2022-09-29 12:56:42 -0400

    Update scheduler.c

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,6 +1,21 @@
+/* ---------------- Nucleus Initialization ------------------------
+ * Written by Kate Plas and Travis Wahl
+ * For CSCI-320 Operating Systems
+ *
+ * The scheduler ensures that every ready process has an opportunity to execute. It is a preemptive round robin algorithm with a time slice value of 5 miliseconds
+ *
+ */
+
 #include "../h/types.h"
 #include "../h/const.h"
 #include "../h/pcb.h"
 #include "../h/asl.h"
+#include "../h/nucleusInitialization.h"
+#include "../h/scheduler.h"
 
-/* The scheduler ensures that every ready process has an opportunity to execute. The scheduler is a preemptive round robin algorithm */
+
+void scheduler() {
+
+
+
+}
