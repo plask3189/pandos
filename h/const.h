4ee49b3801f116433cd7576b970b65dce997c306
Travis Wahl <twahl1991@gmail.com>
2022-10-22 15:31:36 -0400

    Update const.h

--- h/const.h
+++ h/const.h
@@ -32,6 +32,8 @@
 
 
 /* Device constants */
+#define PLT       1
+#define INTERVAL  2
 #define DISK      3
 #define FLASH     4
 #define NETWORK   5
@@ -39,11 +41,13 @@
 #define TERMINAL  7
 
 /* Device Interrupt constants */
-#define DISKINT   0x00000800
-#define FLASHINT  0x00001000
-#define NETWINT   0x00002000
-#define PRNTINT   0x00004000
-#define TERMINT   0x00008000
+#define PLTINT      0x00000200
+#define INTERVALINT 0x00000400
+#define DISKINT     0x00000800
+#define FLASHINT    0x00001000
+#define NETWINT     0x00002000
+#define PRNTINT     0x00004000
+#define TERMINT     0x00008000
 
 #define NUMBEROFDEVICES 49 /* p. 28 of pops says there are 40 device registers in uMPS. On p. 41 of pops, it says there are 8 serial terminal device interfaces. Plus 1 for timer*/
 
