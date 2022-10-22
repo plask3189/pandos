e72e9c1067d9a6a3b2dc8c89dc8472508017b9d8
Travis Wahl <twahl1991@gmail.com>
2022-10-22 14:16:01 -0400

    Update const.h
    added device constants and device interrupts

--- h/const.h
+++ h/const.h
@@ -30,12 +30,20 @@
 
 #define NULL 			    ((void *)0xFFFFFFFF)
 
-/* device interrupts */
-#define DISKINT			  3
-#define FLASHINT 		  4
-#define NETWINT 		  5
-#define PRNTINT 		  6
-#define TERMINT			  7
+
+/* Devices */
+#define DISK      3
+#define FLASH     4
+#define NETWORK   5
+#define PRINTER   6
+#define TERMINAL  7
+
+/* Device Interrupts */
+#define DISKINT			  0x00000800
+#define FLASHINT 		  0x00001000
+#define NETWINT 		  0x00002000
+#define PRNTINT 		  0x00004000
+#define TERMINT			  0x00008000
 
 #define NUMBEROFDEVICES 49 /* p. 28 of pops says there are 40 device registers in uMPS. On p. 41 of pops, it says there are 8 serial terminal device interfaces. Plus 1 for timer*/
 
