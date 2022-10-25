c09c99d3c0bb37891beb3dabb396b9722ccc79ba
Travis Wahl <twahl1991@gmail.com>
2022-10-24 23:29:57 -0400

    Update interrupts.c
    some refactoring and additions

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -14,6 +14,14 @@
 #include "../h/scheduler.h"
 #include "../h/interrupts.h"
 
+/* Inclusions from Initial.c */
+extern int processCount;
+extern int softBlockCount;
+extern pcb_PTR readyQueue;
+extern pcb_PTR currentProcess;
+extern int deviceSemaphores[NUMBEROFDEVICES];
+extern cpu_t startTimeOfDayClock;
+
 void InterruptHandler() {
   /* Variable and STCK creation */
   cpu_t stopTimer;
@@ -21,31 +29,48 @@ void InterruptHandler() {
   
   STCK (stopTimer);
   timeRemaining = getTIMER();
+  unsigned int CAUSE = ((state_PTR)BIOSDATAPAGE) -> s_cause;
   
+  /* If PLT Interrupt? */
+  if ((CAUSE & PLTINT) != 0) {
+    pltInterruptHandler(stopTimer)
+  }
   
   /* If DISK Interrupt? */
-  if ((((state_PTR)BIOSDATAPAGE) -> s_cause & DISKINT) != 0) {
+  if ((CAUSE & DISKINT) != 0) {
     deviceInterruptHandler(DISK);
   }
   
   /* If FLASH Interrupt? */
-  if ((((state_PTR)BIOSDATAPAGE) -> s_cause & FLASHINT) != 0) {
+  if ((CAUSE & FLASHINT) != 0) {
     deviceInterruptHandler(FLASH);
   }
   
   /* If PRINTER Interrupt? */
-  if ((((state_PTR)BIOSDATAPAGE) -> s_cause & PRINTERINT) != 0) {
+  if ((CAUSE & PRINTERINT) != 0) {
     deviceInterruptHandler(PRINTER);
   }
   
   /* If Terminal Interrupt? */
-  if ((((state_PTR)BIOSDATAPAGE) -> s_cause & TERMINT) != 0) {
+  if ((CAUSE & TERMINT) != 0) {
     deviceInterruptHandler(TERMINAL);
   }
-  
-
 }
 
+
+
+/* Interrupt Handler specifically for PLT */
+void pltInterruptHandler(int stopTimer){
+  if(currentProcess != NULL) {
+    currentProcess -> p_time = currentProcess -> p_time + (stopTimer - startTimeOfDayClock)
+    stateStoring((state_PTR)BIOSDATAPAGE, &(currentProcess -> p_s));
+    insertProcQ(&readyQueue, currentProcess);
+    scheduler();    
+    }
+    else {
+      PANIC();
+    }
+}
 
 /* Interrupt Handler specifically for Devices */
 void deviceInterruptHandler(int lineNum) {
@@ -53,7 +78,7 @@ void deviceInterruptHandler(int lineNum) {
   unsigned int status;
   volatile devregarea_t *dReg;
   int devNum;
-  int devSema4;
+  int sema4_d;
   
   dReg = (devregarea_t *) RAMBASEADDR;
   bitM = dReg -> interrupt_dev[lineNum - DISK];
@@ -115,3 +140,18 @@ int terminalInterruptHandler(int sema4_d){
   return (status);
 }
   
+/* Function to help with storing the processor state */
+void stateStoring(state_t *stateObtained, state_t *stateStored) {
+  int i:
+  for (i = 0; i < STATEREGNUM; i++) {
+    stateStored -> s_reg[i] = stateObtained -> s_reg[i];
+  }
+  
+  /* Begin shifting the old states to the new states */
+  stateStored -> s_entryHI = stateObtained -> s_entryHI;
+  stateStored -> s_cause = stateObtained -> s_cause;
+  stateStored -> s_status = stateObtained -> s_status;
+  stateStored -> s_pc = stateObtained -> s_pc;
+}
+  
+
