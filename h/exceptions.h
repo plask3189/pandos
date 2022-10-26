3444efe71d5954fe7d31a30a7f9e797b4b76a6ef
Travis Wahl <twahl1991@gmail.com>
2022-10-26 19:01:14 -0400

    Update exceptions.h

--- h/exceptions.h
+++ h/exceptions.h
@@ -2,6 +2,9 @@
 #define EXCEPTIONS
 
 #include "../h/types.h"
+#include "../h/const.h"
+
+extern void SYSCALLExceptionHandler();
 extern void stateCopy(state_PTR pointerToOldState, state_PTR pointertoNewState);
 
 #endif
