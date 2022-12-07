39036f85b58699e93e57865f274aa0dc5cd449f4
Travis Wahl <twahl1991@gmail.com>
2022-12-07 17:51:01 -0500

    Update initProc.h

---
+++ h/initProc.h
@@ -0,0 +1,12 @@
+#ifndef INITPROC
+#define INITPROC
+
+#include "../h/const.h"
+#include "../h/types.h"
+
+extern int devSem[DEVCOUNT + DEVPERINT];
+extern int masterSema4;
+
+extern void test();
+
+#endif
