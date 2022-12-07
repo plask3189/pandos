54a8711d62ea8702e27589b2e96b6ee4c9b3bafa
Travis Wahl <twahl1991@gmail.com>
2022-12-07 17:50:19 -0500

    Update vmSupport.h

--- h/vmSupport.h
+++ h/vmSupport.h
@@ -1,4 +1,15 @@
 #ifndef VMSUPPORT
 #define VMSUPPORT
 
+#include "../h/const.h"
+#include "../h/types.h"
+
+extern int devSem[DEVCOUNT + DEVPERINT];
+extern int masterSema4;
+
+extern void initTLB();
+extern void uTLBRefillHandler();
+extern void pager();
+
+
 #endif
