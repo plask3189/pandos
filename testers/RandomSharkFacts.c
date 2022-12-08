c88b28a8ebe605447db7eecf91bc74efd13fee39
Travis Wahl <twahl1991@gmail.com>
2022-12-07 20:39:01 -0500

    Update RandomSharkFacts.c

--- testers/RandomSharkFacts.c
+++ testers/RandomSharkFacts.c
@@ -2,9 +2,9 @@
 /* Using a randomizer, this program will randomly select a cool fact about sharks
  * then write it to the terminal using WRITETERMINAL, written by Kate Plas & Travis Wahl */
  
- #include "../h/locallibumps.h"
- #include "../h/tconst.h"
- #include "../h/print.h"
+#include "h/localLibumps.h"
+#include "h/tconst.h"
+#include "h/print.h"
  
  #include <stdlib.h>
  
@@ -14,21 +14,19 @@ void main() {
  
  /* Return a random fact based on the number generated! */
   switch(r){
-    case 0-5:
+    case 0 ... 5:
       print(WRITETERMINAL, "Sharks do not have bones \n");
       break;
     
-    case 6-10:
+    case 6 ... 10:
       print(WRITETERMINAL, "Sharks have electroreceptor organs called ampullae of Lorenzini \n");
       break;
     
-    case 11-15:
+    case 11 ... 15:
       print(WRITETERMINAL, "Each Whale Shark's spots are a unique pattern, like a fingerprint \n");
       break;
     
-    case 16-19:
+    default:
       print(WRITETERMINAL, "There are over 500 species of sharks \n");
-      break;	
   }
- 
  }
